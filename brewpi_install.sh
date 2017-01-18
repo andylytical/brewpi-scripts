2c9d76f8317994d6e1ab5449e3d8887201ef3cdd
Andy Loftus <aloftus@gmail.com>
2017-01-18 02:17:59 -0600

    No required pkgs for brewpi_install

--- brewpi_install.sh
+++ brewpi_install.sh
@@ -4,7 +4,6 @@ DEBUG=1
 BASE=/tmp/brewpi-install
 BREWPI_BASE=$BASE/brewpi-tools
 BREWPI_URL=https://github.com/BrewPi/brewpi-tools.git
-REQUIRED_PGKS=( git )
 
 
 function die() {
@@ -15,11 +14,6 @@ function die() {
 
 # Expect this script to run as root
 [[ $(id -u) -eq 0 ]] || die "Run this script as root"
-
-
-# Ensure required packages
-aptitude -q -y update
-aptitude -q -y install "${REQUIRED_PKGS[@]}" || die "required packages"
 
 
 # Install brewpi
